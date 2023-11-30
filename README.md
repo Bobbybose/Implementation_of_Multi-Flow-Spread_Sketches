8afb52236792e8503f8c2916dea27ea70b81bcae
Bobby Bose <bobbybose19@gmail.com>
2023-11-29 19:33:53 -0500

    Added README

---
+++ README.md
@@ -0,0 +1,82 @@
+# CS685 - Internet Data Streaming 
+# Project 5 - Implementation of Multi-Flow Spread Sketches
+**Author:** Bobby Bose
+
+## Description
+- This is an implementation of a Virtual Bitmap and bSkt (HLL)
+- There are two Python scripts, one for structure
+- An example output is given in two output (.out) files
+- One of the hash functions used in the code was based off of the folding method from https://www.herevego.com/hashing-python/
+
+## Required Packages and Modules
+- No external packages required 
+- The only required modules are the sys, random, and math modules built into Python
+
+## Virtual Bitmap
+- To run, do 'python ./Virtual_Bitmap.py input_file'
+- **Architecture Operation:**
+    - Physical bitmap has 500,000 number of bits
+    - Each virtual bitmap has 500
+        - Each flow is given a virtual bitmap
+        - Each entry in the virtual bitmap hashes to an entry in the physical bitmap
+    - Elements are hashed to one entry in virtual and physical bitmap
+    - When querying, estimate flow spread using formula n_f = l*ln(V_B) - l*ln(V_f)
+        - l = number of bits in the virtual bitmap
+        - V_B = percent of bits in physical bitmap set to 0
+        - V_f = percent of bits in virtual bitmap set to 0
+- **Program Flow**
+    - Read in flows and flow spreads from input file
+    - Create physical bitmap and a virtual bitmap per flow
+    - Use hashing to map indices from virtual bitmap to physical bitmap
+    - Record all flows into virtual and physical bitmaps
+        - Create elements (# elements = flow spread size)
+        - Record element in virtual and physical bitmap using hashing
+    - Query all flows to get estimated flow sizes
+    - Plot actual vs estimated flow sizes
+- **Output:**
+    - Plot of actual vs estimated bit sizes
+        - x axis limit set to flow size = 500
+
+## Probabilistic Bitmap
+- To run, do 'python ./probabilistic_bitmap.py number_bits_in_bitmap sampling_probability'
+- **Architecture Operation:**
+    - Bitmap has a given number of bits
+    - Each element is hashed to one bit in the bitmap based on a given sampling probability
+        - If hashed value < maximum hash value * sampling probability, then hash to an entry using another hash
+    - When querying, estimate flow spread using formula: n_f = -(m/p)ln(v)
+        - m = # bits in bitmap
+        - v = fraction of bits in bitmap that are zeros
+        - p = sampling probability
+- **Program Flow**
+    - Generate 5 flows with spreads 100, 1000, 10000, 100000, 1000000
+    - Records each flow in bitmap and then queries for estimated flow spread
+    - Bitmap resets after each record and query
+- **Output:**
+    - Line for each flow with true flow spread and estimated flow spread
+
+## bSkt (HLL)
+- To run, do 'python ./bSkt_HLL.py input_file'
+- **Architecture Operation:**
+    - Bloom sketch has 4000 HLL estimators
+        - Each HLL estimator has 128 5-bit registers
+    - Flows are hashed to three HLL estimators
+        - Elements are hashed to a register
+    - Value in register is highest geometric mean of an element hashed to the register so far
+    - When querying, estimate flow spread using formula n_f = alpha* l^2 * sum(2^-A[H_i(f)][j] from 0 <= j <= l-1)^-1
+        - l = number of registers in each HLL estimator
+        - alpha = 0.7213/(1 + 1.079/l)
+        - H_i(f) = ith hash function for flow, with 0 < i <= 3
+        - A = bloom sketch
+            - A[H_i(f)] = ith HLL estimator flow f hashes and records elements to
+            - A[H_i(f)][j] = jth register in HLL estimator A[H_i(f)]
+- **Program Flow**
+    - Read in flows and flow spreads from input file
+    - Create bloom sketch
+    - Create hashes for flow hashing
+    - Record all flows in bloom sketch
+        - Create elements (# elements = flow spread size)
+        - Each element hashes to one register in each HLL estimator that flow hashes to
+    - Query all flows to get estimated flow sizes
+    - Print 25 highest estimated flow sizes with flow id
+- **Output:**
+    - 25 highest estimated flow sizes, with associated flow id
--- bSkt_HLL.py
+++ bSkt_HLL.py
@@ -1,6 +1,5 @@
 import sys
 import random
-import math
 
 def main():
     
@@ -37,12 +36,12 @@ def main():
         # Each flow is recorded in list as tuple of (flow id, flow spread, flow id without .'s)
         flows.append((flow_id, num_elements, flow_id_concat_parts))
 
-
+    # Bloom sketch parameters
     num_HLL_estimators = 4000
     num_registers_per_estimator = 128
     num_hashes = 3
 
-    # bloom sketch consisting of a number of HLL estimators, each with a number of 5-bit registers
+    # Bloom sketch consisting of a number of HLL estimators, each with a number of 5-bit registers
     bSketch = []
 
     # Adding HLL estimators to bSketch
@@ -54,20 +53,21 @@ def main():
     for hash in range(num_hashes):
         hashes.append(random.randrange(1000000000))
 
+    # Recording all flows in bloom sketch
     record_flows(flows, bSketch, hashes)
 
+    # Estimating flow spreads in bloom sketch
     estimated_spreads = query_flows(flows, bSketch, hashes)
 
-    for flow_index in range(len(flows)):
-        print("Flow id: " + flows[flow_index][0] + "      Actual flow spread: " + str(flows[flow_index][1]) + "        Estimated spread: " + str(estimated_spreads[flow_index]))
+    #for flow_index in range(len(flows)):
+    #    print("Flow id: " + flows[flow_index][0] + "      Actual flow spread: " + str(flows[flow_index][1]) + "        Estimated spread: " + str(estimated_spreads[flow_index]))
 
-
-    test_flow_information = []
-    for index in range(num_flows):
-        test_flow_information.append((flows[index][1], estimated_spreads[index], flows[index][0]))
-    test_flow_information.sort(reverse = True)
-    for index in range(25):
-        print("aFlow id: " + str(test_flow_information[index][2]) + "      Estimated Size: " + str(test_flow_information[index][1]) + "      Actual Size: " + str(test_flow_information[index][0]))
+    #test_flow_information = []
+    #for index in range(num_flows):
+    #    test_flow_information.append((flows[index][1], estimated_spreads[index], flows[index][0]))
+    #test_flow_information.sort(reverse = True)
+    #for index in range(25):
+    #    print("aFlow id: " + str(test_flow_information[index][2]) + "      Estimated Size: " + str(test_flow_information[index][1]) + "      Actual Size: " + str(test_flow_information[index][0]))
 
     # Refining flow information for output
     flow_information = []
@@ -79,13 +79,13 @@ def main():
 
     # Printing 25 largest estimated flow spreads:
     for index in range(25):
-        print("Flow id: " + str(flow_information[index][1]) + "      Estimated Size: " + str(flow_information[index][0]) + "      Actual Size: " + str(flow_information[index][2]))
+        print("Flow id: " + str(flow_information[index][1]) + "      Estimated Size: " + str(flow_information[index][0]))
+# main()
 
 
-
-# Inputs: List of all flows, physical bitmap, virtual bitmaps
+# Inputs: List of all flows, bloom sketch, hashes
 # Returns: None
-# Description: Generates elements for each flow based on given spread, and inserts them into the flow's virtual bitmap and overall physical bitmap
+# Description: Generates elements for each flow based on given spread, and records them into given number of HLL estimators in bloom sketch
 def record_flows(flows, bSketch, hashes):
     # Recording each flow
     for flow_index in range(len(flows)):
@@ -105,29 +105,23 @@ def record_flows(flows, bSketch, hashes):
             # Getting the geometric hash for this element (0-31)
             geo_hash_value = geometric_hash(element)
 
+            # Hashing to a given number of HLL estimators
             for hash in hashes:
-                # Hash to an estimator using value: flow_id ^ current_hash
+                # Hash to an estimator using value: flow_id ^ hash
                 estimator_hashed_to = hash_function(flows[flow_index][2] ^ hash, 5, len(bSketch))
 
                 # Recording element spread
                 # Number stored in register is maximum between currently stored value, and result of this element's geometric hash
                 bSketch[estimator_hashed_to][register_hashed_to] = max(bSketch[estimator_hashed_to][register_hashed_to], geo_hash_value)
-                #print(bSketch[estimator_hashed_to][register_hashed_to])
-
-                # Register to hash size in
-                #register_to_hash_size = random.randrange(1000000000) % len(bSketch[0])
-
-                # Geometric hash of the register hashing size to
-                #size_geo_hash_value = geometric_hash(register_to_hash_size)
-
-                # Recording packet size
-                #bSketch[estimator_hashed_to][register_to_hash_size] = max(bSketch[estimator_hashed_to][register_to_hash_size], size_geo_hash_value)
 # record_flows()
 
 
+# Inputs: List of all flows, bloom sketch, hashes
+# Returns: None
+# Description: Finds the estimated flow spread for each given flow
 def query_flows(flows, bSketch, hashes):
+    # Variables to calculate spread
     num_registers = len(bSketch[0])
-    
     alpha = 0.7213/(1 + 1.079/num_registers)
 
     # Estimated spreads of all flows
@@ -135,7 +129,7 @@ def query_flows(flows, bSketch, hashes):
 
     # For each flow
     for flow_index in range(len(flows)):
-        # Estimates for this flow from each hashed to estimator
+        # Estimates for this flow from each hashed to HLL estimator
         flow_estimates = []
 
         # Going hash by hash
@@ -153,56 +147,60 @@ def query_flows(flows, bSketch, hashes):
             flow_estimate_for_estimator = alpha * (num_registers**2) * current_estimator_estimate
             flow_estimates.append(flow_estimate_for_estimator)
 
-        #print(flow_estimates)
         # Using minimum estimated flow spread
-        estimated_spread_for_flow = min(flow_estimates)
-        estimated_spreads.append(estimated_spread_for_flow)
+        estimated_spreads.append(min(flow_estimates))
 
+    # Returning all estimated flow spreads
     return estimated_spreads
+# query_flows()
 
-# Inputs: Id of element to hash, what size parts to split id into, length of bitmap
-# Returns: Index in bitmap where element hashed to
+
+# Inputs: key to hash, what size parts to split key into, size of hash table
+# Returns: Index in hash table to hash key to
 # Description: Folding hash function implementation based from https://www.herevego.com/hashing-python/
-#   Split id into a number of parts based on given step size and then add them together
+#   Split key into a number of parts based on given step size and then add them together
 #   Hash function changes depending on step size
-def hash_function(element_id, step_size, size):
-    # If id is too short than error will occur; fixing here
-    if element_id < 10**(step_size):
-        element_id += 10**(step_size)
+def hash_function(key, step_size, size):
+    # If key is too short than error will occur; fixing here
+    if key < 10**(step_size):
+        key += 10**(step_size)
 
     # Pointer to current position of number
     int_pos = 0
-    # Total sum of the split id
+    # Total sum of the split key
     split_id_sum = 0
     # Creating parts until there's no number left
-    while int_pos < len(str(element_id)):
+    while int_pos < len(str(key)):
         # Making sure index isn't out of bounds
-        if int_pos + step_size < len(str(element_id)):
-            split_id_part = str(element_id)[int_pos:int_pos + step_size]
+        if int_pos + step_size < len(str(key)):
+            split_id_part = str(key)[int_pos:int_pos + step_size]
         else:
-            split_id_part = str(element_id)[int_pos:]
+            split_id_part = str(key)[int_pos:]
         
         # Incrementing number position pointer
         int_pos = int_pos + step_size
         split_id_sum += int(split_id_part)
-    #print(str(element_id) + "     " +  str(split_id_sum) + "      " + str(size) + "         " + str(split_id_sum % size))
+    
+    # Returning position key hashes to in hash table
     return split_id_sum % size
 # hash_function()
 
-def hash_function2(key, table_size):
 
+# Inputs: Key to hash, size of table hashing into
+# Returns: Hash value
+# Description: Uniform prime hashing method
+def hash_function2(key, table_size):
+    # Hash and return value
     return (key * 13) % table_size
     
 
 
-# Inputs: Hash value
+# Inputs: Element to get geometric hash of
 # Returns: Geometric hash of input
-# Description: Calculates the geometric hash of the input value by calculating how many leading 0s are in the binary form of the number
-#              Maximum number of bits is 7 for l = 128
+# Description: Calculates the geometric hash of given element by doing a uniform hash and then checking number of leading 0s
 def geometric_hash(element):
-    # Performing uniform hash for element
+    # Performing uniform hash for element 
     hash_value = hash_function2(element**2, 2**32)
-    #print(hash_value)
 
     # Obtaining binary form of hash value and removing prefix
     binary_form = bin(hash_value)[2:]
@@ -210,7 +208,7 @@ def geometric_hash(element):
     # Calculating how many leading 0s are present
     g = 32 - len(binary_form) + 1
     
-    #Returning geometric hash value
+    # Returning geometric hash value
     return g
 # geometric_hash()
 
